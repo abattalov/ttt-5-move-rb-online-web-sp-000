879f800f53b171e180a590133c6b4db19ea5db49
Alisher Battalov <abattalov14@gmail.com>
2021-05-25 01:01:12 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -5,8 +5,3 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
-
-def input_to_index user_input
-  converted_input = input_to_index(user_input)
-  gets converted_input
-end
